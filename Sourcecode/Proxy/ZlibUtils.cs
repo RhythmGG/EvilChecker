78c68d2f6a0179250568dcf36568f58982a8fa65
RhythmGG <59950099+RhythmGG@users.noreply.github.com>
2022-11-02 18:37:01 +0800

    Add files via upload

---
+++ Sourcecode/Proxy/ZlibUtils.cs
@@ -0,0 +1,28 @@
+﻿using Ionic.Zlib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvilChecker.Properties
+{
+    class ZlibUtils
+    {
+		public static byte[] Compress(byte[] to_compress)
+		{
+			byte[] result;
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				using (ZlibStream zlibStream = new ZlibStream(memoryStream, CompressionMode.Compress))
+				{
+					zlibStream.Write(to_compress, 0, to_compress.Length);
+				}
+				result = memoryStream.ToArray();
+			}
+			return result;
+		}
+
+	}
+}
